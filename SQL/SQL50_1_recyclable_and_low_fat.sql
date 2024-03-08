8c16a8b174b27884d3e8dd05c515c4e5f6d6255c
Bailey Mauss <59042705+baileymauss@users.noreply.github.com>
2024-03-08 14:28:07 -0700

    Create SQL50_1_recyclable_and_low_fat.sql

---
+++ SQL/SQL50_1_recyclable_and_low_fat.sql
@@ -0,0 +1,12 @@
+/* 
+Table: Products
+product_id is the primary key (column with unique values) for this table.
+low_fats is an ENUM (category) of type ('Y', 'N') where 'Y' means this product is low fat and 'N' means it is not.
+recyclable is an ENUM (category) of types ('Y', 'N') where 'Y' means this product is recyclable and 'N' means it is not.
+
+Write a solution to find the ids of products that are both low fat and recyclable.
+Return the result table in any order.
+*/
+SELECT product_id
+FROM Products
+WHERE low_fats = 'Y' AND recyclable = 'Y';
