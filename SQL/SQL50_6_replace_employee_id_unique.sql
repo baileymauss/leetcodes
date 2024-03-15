6cd3df2ba9e260b7a373263a5e89e1a9b81bdf81
Bailey Mauss <59042705+baileymauss@users.noreply.github.com>
2024-03-15 14:09:06 -0700

    Create SQL50_6_replace_employee_id_unique.sql
    Add solution for SQL 50 #6: Replace Employee ID with the Unique Identifier
    LeetCode 1378

---
+++ SQL/SQL50_6_replace_employee_id_unique.sql
@@ -0,0 +1,29 @@
+/* 
+Table: Employees
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| name          | varchar |
++---------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table contains the id and the name of an employee in a company.
+
+Table: EmployeeUNI
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| unique_id     | int     |
++---------------+---------+
+(id, unique_id) is the primary key (combination of columns with unique values) for this table.
+Each row of this table contains the id and the corresponding unique id of an employee in the company.
+
+Problem Statement: Write a solution to show the unique ID of each user, If a user does not have a unique ID replace just show null.
+*/
+
+SELECT u.unique_id AS unique_id, e.name AS name
+FROM EmployeeUNI u
+RIGHT JOIN Employees e ON u.id = e.id;
