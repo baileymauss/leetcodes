bc30d38c123902130dfa876d9eaa0974b34fc175
Bailey Mauss <59042705+baileymauss@users.noreply.github.com>
2024-03-18 14:37:23 -0700

    Create SQL50_7_product_sales_analysis_i.sql
    Add solution to SQL 50 #7: Product Sales Analysis I
    LeetCode 1068

---
+++ SQL/SQL50_7_product_sales_analysis_i.sql
@@ -0,0 +1,36 @@
+/* 
+Table: Sales
+
++-------------+-------+
+| Column Name | Type  |
++-------------+-------+
+| sale_id     | int   |
+| product_id  | int   |
+| year        | int   |
+| quantity    | int   |
+| price       | int   |
++-------------+-------+
+(sale_id, year) is the primary key (combination of columns with unique values) of this table.
+product_id is a foreign key (reference column) to Product table.
+Each row of this table shows a sale on the product product_id in a certain year.
+Note that the price is per unit.
+
+Table: Product
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| product_id   | int     |
+| product_name | varchar |
++--------------+---------+
+product_id is the primary key (column with unique values) of this table.
+Each row of this table indicates the product name of each product.
+
+Problem Statement: Write a solution to report the product_name, year, and price for each sale_id in the Sales table.
+*/
+
+SELECT p.product_name AS product_name, 
+       s.year AS year,
+       s.price AS price
+FROM sales s
+LEFT JOIN Product p ON s.product_id = p.product_id;
