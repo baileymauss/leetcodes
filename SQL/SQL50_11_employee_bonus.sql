162fa4f50893203da7c5bc4c784b7c248394bc15
Bailey Mauss <59042705+baileymauss@users.noreply.github.com>
2024-03-22 11:39:36 -0700

    Create SQL50_11_employee_bonus.sql
    Add solution SQL50 #11: Employee Bonus
    LeetCode 577

---
+++ SQL/SQL50_11_employee_bonus.sql
@@ -0,0 +1,34 @@
+/* 
+Table: Employee
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| empId       | int     |
+| name        | varchar |
+| supervisor  | int     |
+| salary      | int     |
++-------------+---------+
+empId is the column with unique values for this table.
+Each row of this table indicates the name and the ID of an employee in addition to their salary and the id of their manager.
+
+Table: Bonus
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| empId       | int  |
+| bonus       | int  |
++-------------+------+
+empId is the column of unique values for this table.
+empId is a foreign key (reference column) to empId from the Employee table.
+Each row of this table contains the id of an employee and their respective bonus.
+
+Problem Statement: Write a solution to report the name and bonus amount of each employee with a bonus less than 1000. Return the result table in any order.
+Note: We are considering no bonus to be included in a bonus less than 1000
+*/
+
+SELECT e.name, b.bonus
+FROM Employee e
+LEFT JOIN Bonus b ON e.empId = b.empId
+WHERE b.bonus < 1000 OR b.bonus IS NULL;
