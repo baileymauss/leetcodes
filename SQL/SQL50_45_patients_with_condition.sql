06e165a575aa8317386128a8def0dd379f55f54a
Bailey Mauss <59042705+baileymauss@users.noreply.github.com>
2024-05-13 12:15:09 -0700

    Create SQL50_45_patients_with_condition.sql
    Add solution SQL50 #45: Patients With a Condition
    LeetCode 1527

---
+++ SQL/SQL50_45_patients_with_condition.sql
@@ -0,0 +1,19 @@
+/* 
+Table: Patients
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| patient_id   | int     |
+| patient_name | varchar |
+| conditions   | varchar |
++--------------+---------+
+patient_id is the primary key (column with unique values) for this table.
+'conditions' contains 0 or more code separated by spaces. 
+This table contains information of the patients in the hospital.
+
+Problem Statement: Write a solution to find the patient_id, patient_name, and conditions of the patients who have Type I Diabetes. Type I Diabetes always starts with DIAB1 prefix. Return the result table in any order.
+*/
+
+SELECT patient_id, patient_name, conditions
+FROM Patients
+WHERE conditions LIKE 'DIAB1%' OR conditions LIKE '% DIAB1%'
